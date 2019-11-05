43ff113003a617a10c5023fbcb87d7e3bb15f9e6
0 Исабекова Ильмира Сериковна <isis18a@econ.msu.ru>
2019-11-05 11:54:27 +0300

    added week8

--- week8.R
+++ week8.R
@@ -2,11 +2,17 @@ generate_data <- function(n) {
   x1 <- runif(100, 0, 1000)
   x2 <- runif (100, 0, 1000)
   x3 <- runif (100, 100, 1300)
-  y <- 5*x1 + 100*x2 - (x3)^2
+  y <- 5*x1 + 100*x2 - 7*(x3)
   return(data.frame(x1=x1,
                     x2=x2,
                     x3=x3,
                     y=y))
 }
 
-data <- generate_data(10000)
+data <- generate_data(1000)
+
+estimate <- lm(data=data, y~x1+x2+x3) 
+
+evaluate <- function(est, m) {
+  return(c(est[1] < m && m < est[2], est[2] - est[1]))
+}
