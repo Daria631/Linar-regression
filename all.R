757160cdf419a8ef971f9e8182ef1e644a1d8f63
mariakonkina <57386369+mariakonkina@users.noreply.github.com>
2019-11-12 09:57:25 +0300

    all code is added

---
+++ all.R
@@ -0,0 +1,26 @@
+generate_data <- function(n, alpha1, alpha2, alpha3) {
+  x1 <- runif(100, 0, 1000)
+  x2 <- runif (100, 0, 1000)
+  x3 <- runif (100, 100, 1300)
+  y <- alpha1*x1 + alpha2*x2 + alpha3*(x3)^2
+  return(data.frame(x1=x1,
+                    x2=x2,
+                    x3=x3,
+                    y=y))
+}
+
+data <- generate_data(10000, 5, 7, -1)
+
+
+estimate <- lm(data=data, y~x1+x2+x3) 
+
+evaluate <- function(est, alpha1, alpha2, alpha3) {
+  return(c(alpha1 - est[1],  alpha2 - est[2], alpha3 - est[3]))
+}
+
+evaluate
+
+
+
+
+
